bfd45fa95c965ff73fce14f81a4875418f408078
Zuzana Červínová <zuzankacervinova@gmail.com>
2022-04-30 11:52:31 +0200

    objednat a zrušit objdnávku

--- src/index.js
+++ src/index.js
@@ -14,3 +14,26 @@ navLinks.forEach((navLink) => {
     navElm.classList.add('nav-closed');
   });
 });
+
+const orderBtn = document.querySelector('.order-btn');
+let ordered = false;
+
+const cancelOrder = () => {
+  orderBtn.textContent = 'Objednat';
+  const drinkCup = document.querySelector('.drink__cup');
+  drinkCup.classList.remove('drink__cup--selected');
+};
+const makeOrder = () => {
+  orderBtn.textContent = 'Zrušit';
+  const drinkCup = document.querySelector('.drink__cup');
+  drinkCup.classList.add('drink__cup--selected');
+};
+orderBtn.addEventListener('click', (e) => {
+  if (ordered === true) {
+    cancelOrder();
+    ordered = false;
+  } else {
+    makeOrder();
+    ordered = true;
+  }
+});
