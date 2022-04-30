f71b5ba8cb6e7cb5c6ce4b7640d9cbccf02f0dbd
Zuzana Červínová <zuzankacervinova@gmail.com>
2022-04-30 11:27:02 +0200

    zobrazující se a mizející navigace na kliknutí

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './style.css';
 
-console.log('funguju!');
+const navBtn = document.querySelector('#nav-btn');
+
+navBtn.addEventListener('click', (e) => {
+  const navElm = document.querySelector('nav');
+  navElm.classList.toggle('nav-closed');
+});
+
+const navLinks = document.querySelectorAll('.navigation a');
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', (e) => {
+    const navElm = document.querySelector('nav');
+    navElm.classList.add('nav-closed');
+  });
+});
